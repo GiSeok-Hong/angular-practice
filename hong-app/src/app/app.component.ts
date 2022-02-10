b9d109133ea09edaec47b81515b1d84ba7593435
GiSeok-Hong <hgsok3056@naver.com>
2022-02-10 14:45:47 +0900

    @Input 데코레이터 연습

--- hong-app/src/app/app.component.ts
+++ hong-app/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+import { User } from './models/user.model';
+import { UserListComponent } from './user-list/user-list.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'hong-app';
+
+  // 자식 컴포넌트와 공유할 상태 정보
+  usersParents: User[];
+
+  constructor() {
+    this.usersParents = [
+      new User(1, 'hong', 'Administrator'),
+      new User(2, 'park', 'Developer'),
+      new User(3, 'Noh', 'Designer'),
+    ];
+  }
+
+  // 사용자 추가
+  addUser(name: string, role: string) {
+    if (name && role) {
+      this.usersParents = [
+        ...this.usersParents,
+        new User(this.getNextId(), name, role),
+      ];
+    }
+  }
+
+  // 새로운 사용자의 id를 취득
+  getNextId(): number {
+    return this.usersParents.length
+      ? Math.max(...this.usersParents.map(({ id }) => id)) + 1
+      : 1;
+  }
 }
--- hong-app/src/app/user-list/user-list.component.ts
+++ hong-app/src/app/user-list/user-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
+  // @Input 데코레이터 바로 뒤의 프로퍼티명 users와 부모 컴포넌트에서 실행한 프러포티 바인딩의 프로퍼티명 users는 반드시 일치해야 한다.
+  @Input() usersChild!: User[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
