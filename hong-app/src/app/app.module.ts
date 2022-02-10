25b0b34a97cc4eaf35fc479fe2679257a5e3fb6d
GiSeok-Hong <hgsok3056@naver.com>
2022-02-10 14:04:35 +0900

    연습용 component 추가

--- hong-app/src/app/app.module.ts
+++ hong-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
