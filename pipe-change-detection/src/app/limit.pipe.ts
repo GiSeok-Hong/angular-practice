05fd98ca223b878ae8400f01614560e83c84bf64
GiSeok-Hong <hgsok3056@naver.com>
2022-02-14 16:08:43 +0900

    비순수 파이프로 변경

--- pipe-change-detection/src/app/limit.pipe.ts
+++ pipe-change-detection/src/app/limit.pipe.ts
@@ -3,6 +3,9 @@ import { Todo } from './todos.component';
 
 @Pipe({
   name: 'limit',
+
+  // pure 프로퍼티의 값에 따라 순수/비순수 파이프가 정해진다.
+  pure: false, // 비순수 파이프
 })
 export class LimitPipe implements PipeTransform {
   transform(todos: Todo[], limit: number): Todo[] {
