f4627c86e3aa4521c091898ef85eebd851a030bf
GiSeok-Hong <hgsok3056@naver.com>
2022-02-15 10:00:48 +0900

    dependency injection 연습1

---
+++ dependency-injection/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { GreetingService } from './greeting.service';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <button (click)="sayHi()">Say Hi</button>
+    <p>{{ greeting }}</p>
+  `,
+  styles: [],
+})
+export class AppComponent {
+  greeting!: string;
+  greetingService: GreetingService;
+
+  constructor() {
+    // 서비스의 인스턴스를 직접 생성한다.
+    this.greetingService = new GreetingService();
+  }
+  sayHi() {
+    // 서비스의 사용
+    this.greeting = this.greetingService.sayHi();
+  }
+}
