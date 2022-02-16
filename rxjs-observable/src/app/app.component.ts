57fe7e3f1a5bab375f91354415f446f5ac8cd237
GiSeok-Hong <hgsok3056@naver.com>
2022-02-16 14:19:21 +0900

    rxjs-observable 외부환경에 반응하는 옵저버블

--- rxjs-observable/src/app/app.component.ts
+++ rxjs-observable/src/app/app.component.ts
@@ -1,44 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+
+// 1. RxJS 임포트
+import { fromEvent, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: ``,
+  template: `
+    <h3>Mouse Coordinates</h3>
+    <h3>X : {{ posX }} y : {{ posY }}</h3>
+  `,
   styles: [],
 })
 export class AppComponent implements OnInit {
+  mousePosition$!: Observable<Event>;
+  posX = 0;
+  posY: number = 0;
+
   ngOnInit() {
-    // 옵저버블이 구독될 때 호출되는 구독 함수
-    const subscriber = (observer: any) => {
-      try {
-        // next 노티피케이션 방출
-        observer.next(1);
-        observer.next(2);
+    // 2. 옵저버블의 생성(DOM 이벤트를 옵저버블로 변환)
+    this.mousePosition$ = fromEvent(document, 'mousemove');
 
-        // throw new Error('Something wrong!');
-
-        // complete 노티피케이션 방출
-        observer.complete();
-      } catch (e) {
-        // error 노티피케이션 방출
-        observer.error(e);
-      } finally {
-        // 구독 해지될 때 호출되는 콜백 함수
-        return () => console.log('Unsubscribed! 구독 해지!');
-      }
-    };
-
-    // 옵저버블 생성     Observable 변수명 뒤에 $를 붙여주면 스트림통신을 뜻한다.
-    const observable$ = new Observable(subscriber);
-
-    // 구독(Subscription)
-    observable$.subscribe(
-      // 옵저버블이 방출한 next 노티피케이션에 반응하는 next 메소드
-      (value) => console.log('value : ' + value),
-      // 옵저버블이 방출한 error 노티피케이션에 반응하는 error 메소드
-      (error) => console.error('error : ' + error),
-      // 옵저버블이 방출한 complete 노티피케이션에 반응하는 complete 메소드
-      () => console.log('Complete')
+    // 3. 옵저버는 옵저버블을 구독하고 옵저버블이 방출한 데이터를 전파받아 사용한다.
+    this.mousePosition$.subscribe(
+      (event) => {
+        this.posX = (event as MouseEvent).clientX; // 유저가 확실하게 타입을 알때 사용하는게 좋음. 평소에는 사용 안 하는 게 좋음.
+        this.posY = (event as MouseEvent).clientY; // 타입 단언문
+      },
+      (error) => console.log(error),
+      () => console.log('complete!')
     );
   }
 }
