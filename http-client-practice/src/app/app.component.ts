c900dd57f463829a509edc05f2612a143b397b56
GiSeok-Hong <hgsok3056@naver.com>
2022-02-17 11:55:49 +0900

    HttpClient Practice

---
+++ http-client-practice/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Todo {
+  id: number;
+  content: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <ul>
+      <li *ngFor="let todo of todos">{{ todo.content }}</li>
+    </ul>
+    <pre>{{ todos | json }}</pre>
+  `,
+  styles: [],
+})
+export class AppComponent implements OnInit {
+  todos!: Todo[];
+  url = 'http://loaclhost:3000/todos';
+
+  // HttpClient를 컴포넌트에 주입
+  constructor(public http: HttpClient) {}
+
+  ngOnInit() {
+    // HTTP GET 요청
+    // this.http.get(this.url).subscribe(todos => this.todos = todos);
+    /*
+        요청 결과를 프로퍼티에 할당한다.
+        get 메소드는 Observable<Object>를 반환한다.
+        이때 타입이 일치하지 않기 때문에 컴파일 에러가 발생한다.
+      */
+
+    this.http.get<Todo[]>(this.url).subscribe((todos) => (this.todos = todos));
+    //
+  }
+}
