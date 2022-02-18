1ca9a0f9befe5a144616b5e01519c28d85b2b676
GiSeok-Hong <hgsok3056@naver.com>
2022-02-18 11:50:24 +0900

    User-Form 기본 틀 작성

---
+++ user-form/src/app/user-form/user-form.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-form',
+  template: `
+    <h2>User Form</h2>
+    <form #userForm="ngForm" (ngSubmit)="onNgSubmit(userForm)" novalidate>
+      <input type="text" name="userid" placeholder="userid" id="userid" />
+      <input
+        type="password"
+        name="password"
+        placeholder="password"
+        id="password"
+      />
+      <button>submit</button>
+    </form>
+  `,
+  styles: [],
+})
+export class UserFormComponent {
+  onNgSubmit(userForm: NgForm) {
+    console.log(userForm);
+  }
+
+  constructor() {}
+}
