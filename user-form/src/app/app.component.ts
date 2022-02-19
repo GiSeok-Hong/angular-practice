7ff2c15c494afa728e67378fdfcad20a771feac3
giseok <hgsok3056@naver.com>
2022-02-19 13:01:55 +0900

    템플릿 기반 폼 유효성 검증

--- user-form/src/app/app.component.ts
+++ user-form/src/app/app.component.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
     <br /><br /><br />
     <app-counter [(count)]="value"></app-counter>
     <p>Value : {{ value }}</p>
+    <br><br><br>
+    <input type="text" name="title" ngModel #title="ngModel" pattern="[a-zA-Z0-9]{4,10}" required>
+    <em *ngIf="title.errors?.['required'] && title.touched">
+      title을 입력하세요!
+    </em>
+
+    <p>errors:  {{ title.errors | json }}</p>
+    <p>invalid: {{ title.invalid }}</p>
+    <p>dirty:   {{ title.dirty }}</p>
+    <p>touched: {{ title.touched }}</p>
+    <p>pristine: {{ title.pristine }}</p>
   `,
   styles: [],
 })
